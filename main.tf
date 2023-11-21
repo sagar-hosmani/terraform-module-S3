a2f81baade4a64b2a9bfbae6332c06a51f38c099
sagar-hosmani <143168922+sagar-hosmani@users.noreply.github.com>
2023-11-21 14:14:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "terrafrom-s3" {
+resource "aws_s3_bucket" "terraform-s3" {
   bucket = var.bucket
   force_destroy = var.force_destroy
 }
